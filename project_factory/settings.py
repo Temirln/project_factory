97f42c3bc938a6bd4a9d13c91c7c6547d0335031
Temirln <t1mb3rmn@gmail.com>
2023-08-17 03:37:55 +0600

    vercel configuration

--- project_factory/settings.py
+++ project_factory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pk89xiww4ys45nanm_h0z)^^(_bex3vfm@nnqlp()90ya*)==y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
