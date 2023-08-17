741f3594a8982f52b611ea966ab9c361e0d92993
Temirln <t1mb3rmn@gmail.com>
2023-08-17 10:05:19 +0600

    changed vercel settings

--- project_factory/settings.py
+++ project_factory/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     # }
 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': 'EiFhOaJVxnHwpBY7ZtND',
@@ -138,9 +138,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os 
 
 STATIC_URL = "static/"
-STATIC_ROOT = "static/"
+STATICFILES_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
