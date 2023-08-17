518fa5c7c7d7eae62788834da9b5b95ceb4f073d
Temirln <t1mb3rmn@gmail.com>
2023-08-17 23:16:32 +0600

    static

--- project_factory/settings/base.py
+++ project_factory/settings/base.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIR = os.path.join(BASE_DIR, "static")
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
