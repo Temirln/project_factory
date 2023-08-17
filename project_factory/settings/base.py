95b0c96194e08b10123448e96c6b897b6a16965e
Temirln <t1mb3rmn@gmail.com>
2023-08-17 23:20:02 +0600

    Update base.py

--- project_factory/settings/base.py
+++ project_factory/settings/base.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIR = os.path.join(BASE_DIR, "staticfiles_build")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATICFILES_DIR = "static/"
+STATIC_ROOT = "staticfiles_build/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
