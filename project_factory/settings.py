249c74444e44da729a73a707ce4a26100a85440f
Temirln <t1mb3rmn@gmail.com>
2023-08-18 09:56:29 +0600

    Update settings.py

--- project_factory/settings.py
+++ project_factory/settings.py
@@ -142,41 +142,41 @@ ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
 
 WSGI_APPLICATION = "project_factory.wsgi.application"
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'info_file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': 'prod_logs/info.log'
-        },
-        'debug_file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'prod_logs/debug.log'
-        },
-        'error_file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'prod_logs/error.log'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['debug_file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        '': {
-            'handlers': ['error_file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'project_api': {
-            'handlers': ['info_file'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'info_file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': 'prod_logs/info.log'
+#         },
+#         'debug_file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'prod_logs/debug.log'
+#         },
+#         'error_file': {
+#             'level': 'ERROR',
+#             'class': 'logging.FileHandler',
+#             'filename': 'prod_logs/error.log'
+#         }
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['debug_file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         '': {
+#             'handlers': ['error_file'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'project_api': {
+#             'handlers': ['info_file'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         }
+#     },
+# }
