a5e3d4e4bd265f24eee887a408ec080e28265fcf
Temirln <t1mb3rmn@gmail.com>
2023-08-18 09:34:22 +0600

    staticfiles

--- project_factory/urls.py
+++ project_factory/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -40,3 +42,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("project_api.urls")),
 ]
+
+# urlpatterns += static(settings.prod.MEDIA_URL, document_root = settings.prod.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.prod.STATIC_ROOT)
